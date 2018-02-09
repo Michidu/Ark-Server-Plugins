bdda752afe3f2e41e4fcf397e8bf2be9702ece78
Michidu <michido@protonmail.com>
2018-02-09 11:24:52 +0300

    Added tribe motd

---
+++ TribeMotd/TribeMotd/DBHelper.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "TribeMotd.h"
+
+namespace DBHelper
+{
+	inline bool IsEntryExists(int tribe_id)
+	{
+		int count = 0;
+
+		try
+		{
+			auto& db = GetDB();
+
+			db << "SELECT count(1) FROM TribeMotd WHERE TribeId = ?;" << tribe_id >> count;
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected DB error {}", __FILE__, __FUNCTION__, exception.what());
+			return false;
+		}
+
+		return count != 0;
+	}
+}
---
+++ TribeMotd/TribeMotd/TribeMotd.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <Logger/Logger.h>
+
+#include "hdr/sqlite_modern_cpp.h"
+
+sqlite::database& GetDB();
