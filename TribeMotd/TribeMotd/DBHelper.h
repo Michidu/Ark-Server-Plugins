cebea49d58b6382c4cf7b6d8c046d6a006936c0f
Michidu <michido@protonmail.com>
2018-05-11 15:08:48 +0300

    Updated TribeMOTD

--- TribeMotd/TribeMotd/DBHelper.h
+++ TribeMotd/TribeMotd/DBHelper.h
@@ -22,4 +22,23 @@ namespace DBHelper
 
 		return count != 0;
 	}
+	
+	inline bool IsPlayerEntryExists(uint64 steam_id)
+	{
+		int count = 0;
+
+		try
+		{
+			auto& db = GetDB();
+
+			db << "SELECT count(1) FROM PlayerMotd WHERE SteamId = ?;" << steam_id >> count;
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected DB error {}", __FILE__, __FUNCTION__, exception.what());
+			return false;
+		}
+
+		return count != 0;
+	}
 }
--- TribeMotd/TribeMotd/TribeMotd.cpp
+++ TribeMotd/TribeMotd/TribeMotd.cpp
@@ -5,8 +5,8 @@
 
 #include <fstream>
 
-#include "json.hpp"
 #include "DBHelper.h"
+#include "json.hpp"
 
 #pragma comment(lib, "ArkApi.lib")
 
@@ -27,30 +27,51 @@ FString GetText(const std::string& str)
 
 void TribeMotd(AShooterPlayerController* player)
 {
-	FTribeData* tribe = static_cast<AShooterPlayerState*>(player->PlayerStateField()())->
-		MyTribeDataField()();
-
-	if (!tribe)
-		return;
-
-	const int tribe_id = tribe->TribeIDField()();
-	if (tribe_id == 0 || !DBHelper::IsEntryExists(tribe_id))
-		return;
-
+	FString sender;
 	std::string message;
 
-	try
-	{
-		auto& db = GetDB();
-		db << "SELECT Message FROM TribeMotd WHERE TribeId = ?;" << tribe_id >> message;
-	}
-	catch (const sqlite::sqlite_exception& exception)
-	{
-		Log::GetLog()->error("({} {}) Unexpected DB error {}", __FILE__, __FUNCTION__, exception.what());
-		return;
-	}
+	const uint64 steam_id = ArkApi::IApiUtils::GetSteamIdFromController(player);
 
-	const FString tribe_name = tribe->TribeNameField()();
+	if (DBHelper::IsPlayerEntryExists(steam_id))
+	{
+		try
+		{
+			auto& db = GetDB();
+			db << "SELECT Message FROM PlayerMotd WHERE SteamId = ?;" << steam_id >> message;
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected DB error {}", __FILE__, __FUNCTION__, exception.what());
+			return;
+		}
+
+		sender = "Admin";
+	}
+	else
+	{
+		FTribeData* tribe = static_cast<AShooterPlayerState*>(player->PlayerStateField())->
+			MyTribeDataField();
+
+		if (!tribe)
+			return;
+
+		const int tribe_id = tribe->TribeIDField();
+		if (tribe_id == 0 || !DBHelper::IsEntryExists(tribe_id))
+			return;
+
+		try
+		{
+			auto& db = GetDB();
+			db << "SELECT Message FROM TribeMotd WHERE TribeId = ?;" << tribe_id >> message;
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected DB error {}", __FILE__, __FUNCTION__, exception.what());
+			return;
+		}
+
+		sender = tribe->TribeNameField();
+	}
 
 	std::wstring wmsg = ArkApi::Tools::Utf8Decode(message);
 
@@ -61,7 +82,7 @@ void TribeMotd(AShooterPlayerController* player)
 		auto config_color = config["Color"];
 		const FLinearColor color{config_color[0], config_color[1], config_color[2], config_color[3]};
 
-		ArkApi::GetApiUtils().SendServerMessage(player, color, L"{}: {}", *tribe_name, wmsg.c_str());
+		ArkApi::GetApiUtils().SendServerMessage(player, color, L"{}: {}", *sender, wmsg.c_str());
 	}
 	else if (type == "Notification")
 	{
@@ -72,7 +93,7 @@ void TribeMotd(AShooterPlayerController* player)
 		const FLinearColor color{config_color[0], config_color[1], config_color[2], config_color[3]};
 
 		ArkApi::GetApiUtils().SendNotification(player, color, display_scale, display_time,
-		                                       nullptr, L"{}: {}", *tribe_name, wmsg.c_str());
+		                                       nullptr, L"{}: {}", *sender, wmsg.c_str());
 	}
 }
 
@@ -95,8 +116,8 @@ void SetMotd(AShooterPlayerController* player_controller, FString* message, ECha
 			if (!message->RemoveFromStart("/SetMotd "))
 				return;
 
-			FTribeData* tribe = static_cast<AShooterPlayerState*>(player_controller->PlayerStateField()())->
-				MyTribeDataField()();
+			FTribeData* tribe = static_cast<AShooterPlayerState*>(player_controller->PlayerStateField())->
+				MyTribeDataField();
 
 			if (!tribe)
 				return;
@@ -104,8 +125,17 @@ void SetMotd(AShooterPlayerController* player_controller, FString* message, ECha
 			try
 			{
 				auto& db = GetDB();
-				db << "REPLACE INTO TribeMotd (TribeId, Message) VALUES (?, ?);" << tribe->TribeIDField()() << ArkApi::Tools::
-					Utf8Encode(**message);
+
+				const int tribe_id = tribe->TribeIDField();
+				if (DBHelper::IsEntryExists(tribe_id))
+				{
+					db << "UPDATE TribeMotd SET Message = ? WHERE TribeId = ?;" << ArkApi::Tools::Utf8Encode(**message) << tribe_id;
+				}
+				else
+				{
+					db << "INSERT INTO TribeMotd (TribeId, Message) VALUES (?, ?);" << tribe_id
+						<< ArkApi::Tools::Utf8Encode(**message);
+				}
 			}
 			catch (const sqlite::sqlite_exception& exception)
 			{
@@ -119,6 +149,134 @@ void SetMotd(AShooterPlayerController* player_controller, FString* message, ECha
 	else
 	{
 		ArkApi::GetApiUtils().SendChatMessage(player_controller, *GetText("Sender"), *GetText("NotAdmin"));
+	}
+}
+
+void AdminSetTribeMotd(APlayerController* player_controller, FString* cmd, bool)
+{
+	TArray<FString> parsed;
+	cmd->ParseIntoArray(parsed, L" ", true);
+
+	if (parsed.IsValidIndex(2))
+	{
+		if (!cmd->RemoveFromStart(L"SetTribeMotd " + parsed[1]))
+			return;
+
+		try
+		{
+			const int tribe_id = std::stoi(*parsed[1]);
+
+			auto& db = GetDB();
+
+			if (DBHelper::IsEntryExists(tribe_id))
+			{
+				db << "UPDATE TribeMotd SET Message = ? WHERE TribeId = ?;" << ArkApi::Tools::Utf8Encode(**cmd) << tribe_id;
+			}
+			else
+			{
+				db << "INSERT INTO TribeMotd (TribeId, Message) VALUES (?, ?);" << tribe_id
+					<< ArkApi::Tools::Utf8Encode(**cmd);
+			}
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected error {}", __FILE__, __FUNCTION__, exception.what());
+			return;
+		}
+
+		ArkApi::GetApiUtils().SendChatMessage(static_cast<AShooterPlayerController*>(player_controller), *GetText("Sender"),
+		                                      *GetText("ChangedMotd"));
+	}
+}
+
+void AdminSetPlayerMotd(APlayerController* player_controller, FString* cmd, bool)
+{
+	TArray<FString> parsed;
+	cmd->ParseIntoArray(parsed, L" ", true);
+
+	if (parsed.IsValidIndex(2))
+	{
+		if (!cmd->RemoveFromStart(L"SetPlayerMotd " + parsed[1]))
+			return;
+
+		try
+		{
+			const uint64 steam_id = std::stoull(*parsed[1]);
+
+			auto& db = GetDB();
+
+			if (DBHelper::IsPlayerEntryExists(steam_id))
+			{
+				db << "UPDATE PlayerMotd SET Message = ? WHERE SteamId = ?;" << ArkApi::Tools::Utf8Encode(**cmd) << steam_id;
+			}
+			else
+			{
+				db << "INSERT INTO PlayerMotd (SteamId, Message) VALUES (?, ?);" << steam_id
+					<< ArkApi::Tools::Utf8Encode(**cmd);
+			}
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected error {}", __FILE__, __FUNCTION__, exception.what());
+			return;
+		}
+
+		ArkApi::GetApiUtils().SendChatMessage(static_cast<AShooterPlayerController*>(player_controller), *GetText("Sender"),
+		                                      *GetText("ChangedMotd"));
+	}
+}
+
+void AdminRemoveTribeMotd(APlayerController* player_controller, FString* cmd, bool)
+{
+	TArray<FString> parsed;
+	cmd->ParseIntoArray(parsed, L" ", true);
+
+	if (parsed.IsValidIndex(1))
+	{
+		try
+		{
+			const int tribe_id = std::stoi(*parsed[1]);
+			if (DBHelper::IsEntryExists(tribe_id))
+			{
+				auto& db = GetDB();
+				db << "DELETE FROM TribeMotd WHERE TribeId = ?;" << tribe_id;
+			}
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected error {}", __FILE__, __FUNCTION__, exception.what());
+			return;
+		}
+
+		ArkApi::GetApiUtils().SendChatMessage(static_cast<AShooterPlayerController*>(player_controller), *GetText("Sender"),
+		                                      *GetText("RemovedMotd"));
+	}
+}
+
+void AdminRemovePlayerMotd(APlayerController* player_controller, FString* cmd, bool)
+{
+	TArray<FString> parsed;
+	cmd->ParseIntoArray(parsed, L" ", true);
+
+	if (parsed.IsValidIndex(1))
+	{
+		try
+		{
+			const uint64 steam_id = std::stoull(*parsed[1]);
+			if (DBHelper::IsPlayerEntryExists(steam_id))
+			{
+				auto& db = GetDB();
+				db << "DELETE FROM PlayerMotd WHERE SteamId = ?;" << steam_id;
+			}
+		}
+		catch (const sqlite::sqlite_exception& exception)
+		{
+			Log::GetLog()->error("({} {}) Unexpected error {}", __FILE__, __FUNCTION__, exception.what());
+			return;
+		}
+
+		ArkApi::GetApiUtils().SendChatMessage(static_cast<AShooterPlayerController*>(player_controller), *GetText("Sender"),
+		                                      *GetText("RemovedMotd"));
 	}
 }
 
@@ -155,6 +313,11 @@ void Load()
 		"TribeId integer not null,"
 		"Message text"
 		");";
+	db << "create table if not exists PlayerMotd ("
+		"Id integer primary key autoincrement not null,"
+		"SteamId integer not null,"
+		"Message text"
+		");";
 	db << "CREATE UNIQUE INDEX IF NOT EXISTS IdxTribeId ON TribeMotd (TribeId);";
 
 	ArkApi::GetHooks().SetHook("AShooterPlayerController.ServerReadMessageOFTheDay_Implementation",
@@ -162,6 +325,10 @@ void Load()
 	                           &AShooterPlayerController_ServerReadMessageOFTheDay_Impl_original);
 
 	ArkApi::GetCommands().AddChatCommand("/SetMotd", &SetMotd);
+	ArkApi::GetCommands().AddConsoleCommand("SetTribeMotd", &AdminSetTribeMotd);
+	ArkApi::GetCommands().AddConsoleCommand("SetPlayerMotd", &AdminSetPlayerMotd);
+	ArkApi::GetCommands().AddConsoleCommand("RemoveTribeMotd", &AdminRemoveTribeMotd);
+	ArkApi::GetCommands().AddConsoleCommand("RemovePlayerMotd", &AdminRemovePlayerMotd);
 }
 
 void Unload()
@@ -170,6 +337,10 @@ void Unload()
 	                               &Hook_AShooterPlayerController_ServerReadMessageOFTheDay_Impl);
 
 	ArkApi::GetCommands().RemoveChatCommand("/SetMotd");
+	ArkApi::GetCommands().RemoveConsoleCommand("SetTribeMotd");
+	ArkApi::GetCommands().RemoveConsoleCommand("SetPlayerMotd");
+	ArkApi::GetCommands().RemoveConsoleCommand("RemoveTribeMotd");
+	ArkApi::GetCommands().RemoveConsoleCommand("RemovePlayerMotd");
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
