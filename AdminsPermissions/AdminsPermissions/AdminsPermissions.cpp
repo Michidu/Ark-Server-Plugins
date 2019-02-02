72519b39892472ebdde6ac384242b06aaa0882b7
Michidu <michido@protonmail.com>
2019-02-02 18:49:29 +0300

    Atlas support for AdminsPermissions

--- AdminsPermissions/AdminsPermissions/AdminsPermissions.cpp
+++ AdminsPermissions/AdminsPermissions/AdminsPermissions.cpp
@@ -1,20 +1,48 @@
-#include <API/ARK/Ark.h>
-#include <API/UE/Math/ColorList.h>
-#include <Logger/Logger.h>
-#include <Permissions.h>
+#ifdef PERMISSIONS_ARK
+#include <ArkPermissions.h>
+#else
+#include <AtlasPermissions.h>
+#endif
 
 #include <fstream>
 
 #include "json.hpp"
 
+#ifdef PERMISSIONS_ARK
 #pragma comment(lib, "ArkApi.lib")
+#else
+#pragma comment(lib, "AtlasApi.lib")
+#endif
+
 #pragma comment(lib, "Permissions.lib")
 
 nlohmann::json config;
 
+std::string GetConfigPath()
+{
+#ifdef PERMISSIONS_ARK
+	return ArkApi::Tools::GetCurrentDir() + "/ArkApi/Plugins/AdminsPermissions/config.json";
+#else
+	return ArkApi::Tools::GetCurrentDir() + "/AtlasApi/Plugins/AdminsPermissions/config.json";
+#endif
+}
+
 FString GetText(const std::string& str)
 {
 	return FString(ArkApi::Tools::Utf8Decode(config["Messages"].value(str, "No message")).c_str());
+}
+
+void ConsoleCommand(APlayerController* player, FString command)
+{
+	const bool is_admin = player->bIsAdmin()(), is_cheat = player->bCheatPlayer()();
+	player->bIsAdmin() = true;
+	player->bCheatPlayer() = true;
+
+	FString result;
+	player->ConsoleCommand(&result, &command, true);
+
+	player->bIsAdmin() = is_admin;
+	player->bCheatPlayer() = is_cheat;
 }
 
 void OnChatMessage(AShooterPlayerController* player_controller, FString* message, EChatSendMode::Type)
@@ -25,15 +53,14 @@ void OnChatMessage(AShooterPlayerController* player_controller, FString* message
 	if (!parsed.IsValidIndex(1))
 		return;
 
-	const uint64 steam_id = ArkApi::GetApiUtils().GetSteamIdFromController(player_controller);
+	const uint64 steam_id = ArkApi::IApiUtils::GetSteamIdFromController(player_controller);
 
 	if (Permissions::IsPlayerHasPermission(steam_id, "Cheat." + parsed[1]))
 	{
 		if (!message->RemoveFromStart("/cheat "))
 			return;
 
-		FString result;
-		player_controller->ConsoleCommand(&result, message, true);
+		ConsoleCommand(player_controller, *message);
 
 		FString log = FString::Format(*GetText("LogMsg"), *ArkApi::IApiUtils::GetCharacterName(player_controller),
 		                              **message);
@@ -52,7 +79,7 @@ void OnChatMessage(AShooterPlayerController* player_controller, FString* message
 
 void ReadConfig()
 {
-	const std::string config_path = ArkApi::Tools::GetCurrentDir() + "/ArkApi/Plugins/AdminsPermissions/config.json";
+	const std::string config_path = GetConfigPath();
 	std::ifstream file{config_path};
 	if (!file.is_open())
 		throw std::runtime_error("Can't open config.json");
@@ -84,7 +111,7 @@ void Unload()
 	ArkApi::GetCommands().RemoveChatCommand("/cheat");
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE, DWORD ul_reason_for_call, LPVOID)
 {
 	switch (ul_reason_for_call)
 	{
