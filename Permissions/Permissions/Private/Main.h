dd255eb2e3e8764798b2b273d66e8cd3b0ce5038
Kalendell <matthew.childers1992@gmail.com>
2021-01-13 22:39:43 -0600

    Timed Permissions Tribe permissions PermissionsCallback
    Cleanup and undo include changes and plugin unload

--- Permissions/Permissions/Private/Main.h
+++ Permissions/Permissions/Private/Main.h
@@ -7,4 +7,7 @@ namespace Permissions
 	inline std::unique_ptr<IDatabase> database;
 
 	std::string GetDbPath();
+	int GetTribeId(AShooterPlayerController* playerController);
+	FTribeData* GetTribeData(AShooterPlayerController* playerController);
+	TArray<FString> GetTribeDefaultGroups(FTribeData* tribeData);
 }
